c64118e1412bef3149f80d9e1ce48508be93e791
rabah <rabawinnr@gmail.com>
2026-06-23 11:39:42 +0100

    worked on infinite carousel (failed)

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,28 @@ import './bootstrap';
 import.meta.glob([
     '../fonts/**',
 ]);
+
+window.leftArrow = function () {
+    
+}
+
+
+
+window.rightArrow = function () {
+    const parent = document.getElementById("customersParent");
+    const children = Array.from(parent.children);
+
+    const last = parent.lastElementChild;
+    const first = parent.firstElementChild;
+
+    parent.insertBefore(last, first);
+    
+    
+    children.forEach((v,i) => {
+        v.style.transform = v.style.transform + `translatex(520px)`;
+    })
+    
+
+
+    
+}
