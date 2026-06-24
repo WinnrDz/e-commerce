50641cec7cf8e1481f5b30ebe3759ca5069d7d03
rabah <rabawinnr@gmail.com>
2026-06-24 11:56:04 +0100

    finished infinite carousel left side

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,35 @@ import.meta.glob([
     '../fonts/**',
 ]);
 
-window.leftArrow = function () {
+window.leftArrow = async function () {
+   const parent = document.getElementById("customersParent");
+    const children = Array.from(parent.children);
+
+    const first = parent.firstElementChild
+    const last = parent.lastElementChild
     
+    children.forEach((v,i) => {
+        if (i == 4) v.classList.remove("blur-[1.75px]"); 
+        if (i == 1) v.classList.add("transition-transform","duration-300","ease-in-out", "blur-[1.75px]"); 
+        if (i != 0) {
+        v.classList.add("transition-transform","duration-300","ease-in-out");
+        v.style.transform = v.style.transform + `translatex(-106%)`;
+        } else {
+            v.classList.add("transition-transform","duration-300","ease-in-out" , "blur-[1.75px]");
+            v.style.transform = `translatex(425%)`;
+        }
+    })
+    
+    await new Promise(r => setTimeout(r, 300));
+
+    parent.insertBefore(first, last.nextSibling);
+    
+    children.forEach((v,i) => {
+        v.classList.remove("transition-transform","duration-300","ease-in-out");
+        v.style.transform = "";
+    })
+
+
 }
 
 
