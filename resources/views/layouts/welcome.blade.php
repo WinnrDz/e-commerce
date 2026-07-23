f7072953f5c698aa44c0802a3396dfea493e3e40
rabah <rabawinnr@gmail.com>
2026-07-23 18:20:48 +0100

    made the welcome page responisable

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -17,7 +17,8 @@
     @livewireStyles
 </head>
 
-<body class="bg-white ">
+<body class="bg-white min-w-[1400px]">
+    
     <nav class="flex justify-start items-center gap-10 font-satoshi text-[16px] bg-[#FFFFFF] px-7 py-5">
         <div class="pl-12">
             <h1 class="font-integral font-bold text-[32px] ">SHOP.CO</h1>
@@ -84,15 +85,15 @@
                     </div>
                 </div>
             </div>
-            <div>
-                <img class="w-[663px] m-10 mb-0" src="{{ asset('images/fashion-hero.png') }}" alt="Logo">
-                <svg class="absolute top-30 right-30" width="104" height="104" viewBox="0 0 104 104" fill="none"
+            <div class="relative mt-auto">
+                <img class="w-[640px] mb-0 " src="{{ asset('images/fashion-hero.png') }}" alt="Logo">
+                <svg class="absolute top-30 right-[0%]" width="104" height="104" viewBox="0 0 104 104" fill="none"
                     xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M52 0C53.7654 27.955 76.0448 50.2347 104 52C76.0448 53.7654 53.7654 76.0448 52 104C50.2347 76.0448 27.955 53.7654 0 52C27.955 50.2347 50.2347 27.955 52 0Z"
                         fill="black" />
                 </svg>
-                <svg class="absolute bottom-90 right-200" width="56" height="56" viewBox="0 0 56 56" fill="none"
+                <svg class="absolute bottom-50 right-[100%]" width="56" height="56" viewBox="0 0 56 56" fill="none"
                     xmlns="http://www.w3.org/2000/svg">
                     <path
                         d="M28 0C28.9506 15.0527 40.9472 27.0495 56 28C40.9472 28.9506 28.9506 40.9472 28 56C27.0495 40.9472 15.0527 28.9506 0 28C15.0527 27.0495 27.0495 15.0527 28 0Z"
@@ -699,5 +700,5 @@
             </div>
         
     </footer>
-    </body>
+</body>
 </html>
