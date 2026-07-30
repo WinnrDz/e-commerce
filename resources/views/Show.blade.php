66aa4f04ce8f7c59d9aa53c705ec86b000bffbd3
rabah <rabawinnr@gmail.com>
2026-07-30 13:01:33 +0100

    worked on show vue responsiveness

--- resources/views/Show.blade.php
+++ resources/views/Show.blade.php
@@ -2,53 +2,53 @@
 @section('title', 'Show')
 @section('content')
     <div id="show" class="mb-15 pt-4 mt-0 flex items-center gap-4 ">
-        <a class="font-satoshi text-[16px] text-black opacity-60 cursor-pointer">Home</a>
+        <a class="font-satoshi text-[14px] lg:text-[16px] text-black opacity-60 cursor-pointer">Home</a>
         <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
                 d="M1.28318 0.221294L6.28318 5.22129C6.3531 5.29097 6.40857 5.37377 6.44643 5.46493C6.48428 5.55609 6.50377 5.65383 6.50377 5.75254C6.50377 5.85126 6.48428 5.94899 6.44643 6.04016C6.40857 6.13132 6.3531 6.21412 6.28318 6.28379L1.28318 11.2838C1.14228 11.4247 0.951183 11.5038 0.751926 11.5038C0.552669 11.5038 0.361572 11.4247 0.220676 11.2838C0.0797797 11.1429 0.000625142 10.9518 0.000625142 10.7525C0.000625143 10.5533 0.0797797 10.3622 0.220676 10.2213L4.69005 5.75192L0.220051 1.28255C0.0791551 1.14165 1.25847e-07 0.950553 1.28223e-07 0.751295C1.30599e-07 0.552037 0.0791552 0.360941 0.220051 0.220045C0.360948 0.0791493 0.552044 -7.62281e-06 0.751301 -7.62044e-06C0.950559 -7.61806e-06 1.14166 0.0791493 1.28255 0.220045L1.28318 0.221294Z"
                 fill="black" fill-opacity="0.6" />
         </svg>
-        <a class="font-satoshi text-[16px] text-black opacity-60 cursor-pointer">Shop</a>
+        <a class="font-satoshi text-[14px] lg:text-[16px] text-black opacity-60 cursor-pointer">Shop</a>
         <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
                 d="M1.28318 0.221294L6.28318 5.22129C6.3531 5.29097 6.40857 5.37377 6.44643 5.46493C6.48428 5.55609 6.50377 5.65383 6.50377 5.75254C6.50377 5.85126 6.48428 5.94899 6.44643 6.04016C6.40857 6.13132 6.3531 6.21412 6.28318 6.28379L1.28318 11.2838C1.14228 11.4247 0.951183 11.5038 0.751926 11.5038C0.552669 11.5038 0.361572 11.4247 0.220676 11.2838C0.0797797 11.1429 0.000625142 10.9518 0.000625142 10.7525C0.000625143 10.5533 0.0797797 10.3622 0.220676 10.2213L4.69005 5.75192L0.220051 1.28255C0.0791551 1.14165 1.25847e-07 0.950553 1.28223e-07 0.751295C1.30599e-07 0.552037 0.0791552 0.360941 0.220051 0.220045C0.360948 0.0791493 0.552044 -7.62281e-06 0.751301 -7.62044e-06C0.950559 -7.61806e-06 1.14166 0.0791493 1.28255 0.220045L1.28318 0.221294Z"
                 fill="black" fill-opacity="0.6" />
         </svg>
-        <a class="font-satoshi text-[16px] text-black opacity-60 cursor-pointer">Men</a>
+        <a class="font-satoshi text-[14px] lg:text-[16px] text-black opacity-60 cursor-pointer">Men</a>
         <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
                 d="M1.28318 0.221294L6.28318 5.22129C6.3531 5.29097 6.40857 5.37377 6.44643 5.46493C6.48428 5.55609 6.50377 5.65383 6.50377 5.75254C6.50377 5.85126 6.48428 5.94899 6.44643 6.04016C6.40857 6.13132 6.3531 6.21412 6.28318 6.28379L1.28318 11.2838C1.14228 11.4247 0.951183 11.5038 0.751926 11.5038C0.552669 11.5038 0.361572 11.4247 0.220676 11.2838C0.0797797 11.1429 0.000625142 10.9518 0.000625142 10.7525C0.000625143 10.5533 0.0797797 10.3622 0.220676 10.2213L4.69005 5.75192L0.220051 1.28255C0.0791551 1.14165 1.25847e-07 0.950553 1.28223e-07 0.751295C1.30599e-07 0.552037 0.0791552 0.360941 0.220051 0.220045C0.360948 0.0791493 0.552044 -7.62281e-06 0.751301 -7.62044e-06C0.950559 -7.61806e-06 1.14166 0.0791493 1.28255 0.220045L1.28318 0.221294Z"
                 fill="black" fill-opacity="0.6" />
         </svg>
-        <a class="font-satoshi text-[16px] text-black ">T-shirts</a>
+        <a class="font-satoshi text-[14px] lg:text-[16px] text-black ">T-shirts</a>
     </div>
-    <div class="flex gap-4">
-        <div id="previewCont">
-            <div class="bg-[#F0EEED] w-38 h-41 rounded-[20px] border black mb-8 flex justify-center items-center cursor-pointer"
+    <div class="flex flex-col lg:flex-row gap-4">
+        <div id="previewCont" class="flex flex-row lg:flex-col gap-8 order-2 lg:order-1 h-[106px] lg:h-auto">
+            <div class="bg-[#F0EEED] p-2 w-full max-w-[111px] lg:max-w-38 h-full lg:max-h-41 rounded-[20px] border black flex justify-center items-center overflow-hidden cursor-pointer"
                 onclick="preview(0)">
-                <img class="bg-[#F0EEED] w-34 " src="{{ asset('images/life-shirt.png') }}" alt="Logo">
+                <img src="{{ asset('images/life-shirt.png') }}" alt="Logo">
             </div>
-            <div class="bg-[#F0EEED] w-38 h-41 rounded-[20px] mb-8 flex justify-center items-center cursor-pointer"
+            <div class="bg-[#F0EEED] p-2 w-full max-w-[111px] lg:max-w-38 h-full lg:max-h-41 rounded-[20px] flex justify-center items-center overflow-hidden cursor-pointer"
                 onclick="preview(1)">
-                <img class="bg-[#F0EEED] w-34 " src="{{ asset('images/dead-shirt.png') }}" alt="Logo">
+                <img src="{{ asset('images/dead-shirt.png') }}" alt="Logo">
             </div>
-            <div class="bg-[#F0EEED] w-38 h-41 rounded-[20px] mb-8 flex justify-center items-center overflow-hidden cursor-pointer"
+            <div class="bg-[#F0EEED] p-2 w-full max-w-[111px] lg:max-w-38 h-full lg:max-h-41 rounded-[20px] flex justify-center items-center overflow-hidden cursor-pointer"
                 onclick="preview(2)">
-                <img class="bg-[#F0EEED] w-44 max-w-none translate-x-2" src="{{ asset('images/life-guy.png') }}"
+                <img src="{{ asset('images/life-guy.png') }}"
                     alt="Logo">
             </div>
         </div>
         <div id="zoomable"
-            class="bg-[#F0EEED] w-111 h-140 rounded-[20px] mb-8 flex justify-center items-center overflow-hidden cursor-pointer relative">
-            <img id="previewImage" class="bg-[#F0EEED] w-100 " src="{{ asset('images/life-shirt.png') }}" alt="Logo">
+            class="order-1 lg:order-2 bg-[#F0EEED] w-full max-w-[358px] lg:max-w-111 max-h-[300px] lg:max-h-[555px] rounded-[20px] p-4 mx-auto lg:mx-0 flex justify-center items-center overflow-hidden cursor-pointer relative">
+            <img id="previewImage" class="bg-[#F0EEED] p-4 " src="{{ asset('images/life-shirt.png') }}" alt="Logo">
             <div id="square" class="bg-[#91FFFF] h-70 w-60 absolute z-50 opacity-20 hidden"></div>
         </div>
         <div id="zoomed"
-            class="bg-[#F0EEED] w-154 h-140 rounded-[20px] mb-8 flex justify-center items-center overflow-hidden relative hidden ">
+            class="order-3 bg-[#F0EEED] w-154 h-140 rounded-[20px] flex justify-center items-center overflow-hidden relative hidden ">
             <img id="zoomedImage" class="bg-[#F0EEED] w-100 scale-200 absolute" src="{{ asset('images/life-shirt.png') }}"
                 alt="Logo">
         </div>
-        <div id="buyContent" class="flex flex-col gap-4 ml-4">
+        <div id="buyContent" class="order-3 flex flex-col gap-4 ml-4">
             <h1 class="font-integral font-bold text-[40px] ">ONE LIFE GRAPHIC T-SHIRT</h1>
             <div class="flex gap-2">
                 <svg width="24" height="23" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg">
