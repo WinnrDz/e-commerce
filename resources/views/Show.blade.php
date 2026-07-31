38b9fc82c11196667bdee73f87d43094476ed9a5
rabah <rabawinnr@gmail.com>
2026-07-31 18:15:13 +0100

    finished show vue responsiveness

--- resources/views/Show.blade.php
+++ resources/views/Show.blade.php
@@ -141,28 +141,25 @@
     <div class="flex flex-col w-full max-w-320 mt-15 gap-8">
         <div id="detailBtnsCont" class="flex">
             <button
-                class="w-[40%] lg:w-[33%] font-satoshi text-[15px] lg:text-[20px] opacity-60 border-b border-black/10 leading-[22px] p-2 lg:p-5 cursor-pointer hover:bg-gray-100 transition-colors duration-150 relative overflow-hidden">Product
+                class="w-[45%] lg:w-70 lg:w-[33%] font-satoshi text-[15 px] lg:text-[20px] opacity-60 border-b border-black/10 leading-[22px] p-2 lg:p-5 cursor-pointer hover:bg-gray-100 transition-colors duration-150 relative overflow-hidden">Product
                 Details</button>
             <button
-                class="w-[40%] lg:w-[33%] font-satoshim text-[15px] lg:text-[20px] border-b leading-[22px] p-2 lg:p-5 cursor-pointer hover:bg-gray-100 transition-colors duration-150 relative overflow-hidden">Rating
+                class="w-[45%] lg:w-70 lg:w-[33%] font-satoshim text-[15    px] lg:text-[20px] border-b leading-[22px] p-2 lg:p-5 cursor-pointer hover:bg-gray-100 transition-colors duration-150 relative overflow-hidden">Rating
                 & Reviews</button>
             <button
-                class="w-[20%] lg:w-[33%] font-satoshi text-[15px] lg:text-[20px] opacity-60 border-b border-black/10 leading-[22px] p-2 lg:p-5 cursor-pointer hover:bg-gray-100 transition-colors duration-150 relative overflow-hidden">FAQs</button>
+                class="w-[20%] lg:w-[33%] font-satoshi text-[15 px] lg:text-[20px] opacity-60 border-b border-black/10 leading-[22px] p-2 lg:p-5 cursor-pointer hover:bg-gray-100 transition-colors duration-150 relative overflow-hidden">FAQs</button>
         </div>
-        <div class="flex justify-between">
-            <div class="flex items-center gap-2">
-                <h3 class="font-satoshi font-bold text-[24px]">All Reviews</h3>
-                <span class="font-satoshi text-[14px] lg:text-[16px] leading-[22px] opacity-60">(451)</span>
-            </div>
+        <div class="flex justify-between items-center">
+            <h3 class="font-satoshi font-bold text-[20px] lg:text-[24px] whitespace-nowrap">All Reviews <span class="font-satoshi text-[14px] lg:text-[16px] leading-[22px] opacity-60">(451)</span></h3>
             <div class="flex items-center gap-4">
-                <button class="flex items-center justify-center bg-[#F0F0F0] w-12 h-12 rounded-[62px] cursor-pointer"><svg
+                <button class="flex items-center justify-center bg-[#F0F0F0] w-[40px] lg:w-12 h-[40px] lg:h-12 rounded-[62px] cursor-pointer"><svg
                         width="24" height="24" viewBox="0 0 24 24" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M13.125 11.625V20.25C13.125 20.5484 13.0065 20.8345 12.7955 21.0455C12.5845 21.2565 12.2984 21.375 12 21.375C11.7016 21.375 11.4155 21.2565 11.2045 21.0455C10.9935 20.8345 10.875 20.5484 10.875 20.25V11.625C10.875 11.3266 10.9935 11.0405 11.2045 10.8295C11.4155 10.6185 11.7016 10.5 12 10.5C12.2984 10.5 12.5845 10.6185 12.7955 10.8295C13.0065 11.0405 13.125 11.3266 13.125 11.625ZM18.75 18C18.4516 18 18.1655 18.1185 17.9545 18.3295C17.7435 18.5405 17.625 18.8266 17.625 19.125V20.25C17.625 20.5484 17.7435 20.8345 17.9545 21.0455C18.1655 21.2565 18.4516 21.375 18.75 21.375C19.0484 21.375 19.3345 21.2565 19.5455 21.0455C19.7565 20.8345 19.875 20.5484 19.875 20.25V19.125C19.875 18.8266 19.7565 18.5405 19.5455 18.3295C19.3345 18.1185 19.0484 18 18.75 18ZM21 14.25H19.875V3.75C19.875 3.45163 19.7565 3.16548 19.5455 2.9545C19.3345 2.74353 19.0484 2.625 18.75 2.625C18.4516 2.625 18.1655 2.74353 17.9545 2.9545C17.7435 3.16548 17.625 3.45163 17.625 3.75V14.25H16.5C16.2016 14.25 15.9155 14.3685 15.7045 14.5795C15.4935 14.7905 15.375 15.0766 15.375 15.375C15.375 15.6734 15.4935 15.9595 15.7045 16.1705C15.9155 16.3815 16.2016 16.5 16.5 16.5H21C21.2984 16.5 21.5845 16.3815 21.7955 16.1705C22.0065 15.9595 22.125 15.6734 22.125 15.375C22.125 15.0766 22.0065 14.7905 21.7955 14.5795C21.5845 14.3685 21.2984 14.25 21 14.25ZM5.25 15C4.95163 15 4.66548 15.1185 4.4545 15.3295C4.24353 15.5405 4.125 15.8266 4.125 16.125V20.25C4.125 20.5484 4.24353 20.8345 4.4545 21.0455C4.66548 21.2565 4.95163 21.375 5.25 21.375C5.54837 21.375 5.83452 21.2565 6.0455 21.0455C6.25647 20.8345 6.375 20.5484 6.375 20.25V16.125C6.375 15.8266 6.25647 15.5405 6.0455 15.3295C5.83452 15.1185 5.54837 15 5.25 15ZM7.5 11.25H6.375V3.75C6.375 3.45163 6.25647 3.16548 6.0455 2.9545C5.83452 2.74353 5.54837 2.625 5.25 2.625C4.95163 2.625 4.66548 2.74353 4.4545 2.9545C4.24353 3.16548 4.125 3.45163 4.125 3.75V11.25H3C2.70163 11.25 2.41548 11.3685 2.2045 11.5795C1.99353 11.7905 1.875 12.0766 1.875 12.375C1.875 12.6734 1.99353 12.9595 2.2045 13.1705C2.41548 13.3815 2.70163 13.5 3 13.5H7.5C7.79837 13.5 8.08452 13.3815 8.2955 13.1705C8.50647 12.9595 8.625 12.6734 8.625 12.375C8.625 12.0766 8.50647 11.7905 8.2955 11.5795C8.08452 11.3685 7.79837 11.25 7.5 11.25ZM14.25 6.75H13.125V3.75C13.125 3.45163 13.0065 3.16548 12.7955 2.9545C12.5845 2.74353 12.2984 2.625 12 2.625C11.7016 2.625 11.4155 2.74353 11.2045 2.9545C10.9935 3.16548 10.875 3.45163 10.875 3.75V6.75H9.75C9.45163 6.75 9.16548 6.86853 8.9545 7.0795C8.74353 7.29048 8.625 7.57663 8.625 7.875C8.625 8.17337 8.74353 8.45952 8.9545 8.6705C9.16548 8.88147 9.45163 9 9.75 9H14.25C14.5484 9 14.8345 8.88147 15.0455 8.6705C15.2565 8.45952 15.375 8.17337 15.375 7.875C15.375 7.57663 15.2565 7.29048 15.0455 7.0795C14.8345 6.86853 14.5484 6.75 14.25 6.75Z"
                             fill="black" />
                     </svg></button>
-                <button class="flex items-center justify-around bg-[#F0F0F0] w-30 h-12 rounded-[62px] cursor-pointer">
+                <button class="flex items-center justify-around bg-[#F0F0F0] w-30 h-12 rounded-[62px] cursor-pointer hidden lg:flex">
                     <span class="font-satoshim text-[14px] lg:text-[16px]">Latest</span>
                     <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
@@ -172,12 +169,12 @@
                     </svg>
                 </button>
                 <button
-                    class="flex items-center justify-around bg-black w-42 h-12 rounded-[62px] cursor-pointer font-satoshi font-medium text-[14px] lg:text-[16px] text-white">Write
+                    class="flex items-center justify-around bg-black px-[16px] lg:px-[20px] py-[12px] lg:py-[16px] rounded-[62px] cursor-pointer font-satoshi font-medium text-[14px] lg:text-[16px] text-white">Write
                     a Review</button>
             </div>
         </div>
-        <div class="flex flex-wrap gap-8">
-            <div class="border border-black/10 rounded-[20px] w-152 h-60 p-6 flex flex-col justify-around">
+        <div class="flex flex-wrap gap-y-6 justify-between">
+            <div class="border border-black/10 rounded-[20px] w-[100%] lg:w-[49%] p-6 flex flex-col justify-around">
                 <div class="flex justify-between">
                     <div class="flex gap-2">
                         <svg width="22" height="21" viewBox="0 0 22 21" fill="none"
@@ -229,17 +226,15 @@
                             fill="#01AB31" />
                     </svg>
                 </div>
-
                 <p class="font-satoshi text-[14px] lg:text-[16px] text-black opacity-60">
                     "I absolutely love this t-shirt! The design is unique and the fabric feels so comfortable. As a
                     fellow designer, I appreciate the attention to detail. It's become my favorite go-to shirt."
                 </p>
-                <p class="mt-4 pb-8 font-satoshim text-[14px] lg:text-[16px] text-black opacity-60">
+                <p class="mt-4  font-satoshim text-[14px] lg:text-[16px] text-black opacity-60">
                     Posted on August 14, 2023
                 </p>
-
             </div>
-            <div class="border border-black/10 rounded-[20px] w-152 h-60 p-6 flex flex-col justify-around">
+            <div class="border border-black/10 rounded-[20px] w-[100%] lg:w-[49%] p-6 flex flex-col justify-around">
                 <div class="flex justify-between">
                     <div class="flex gap-2">
                         <svg width="22" height="21" viewBox="0 0 22 21" fill="none"
@@ -297,12 +292,12 @@
                     Being a UI/UX designer myself, I'm quite picky about aesthetics, and this t-shirt definitely gets a
                     thumbs up from me."
                 </p>
-                <p class="mt-4 pb-8 font-satoshim text-[14px] lg:text-[16px] text-black opacity-60">
+                <p class="mt-4  font-satoshim text-[14px] lg:text-[16px] text-black opacity-60">
                     Posted on August 15, 2023
                 </p>
 
             </div>
-            <div class="border border-black/10 rounded-[20px] w-152 h-60 p-6 flex flex-col justify-around">
+            <div class="border border-black/10 rounded-[20px] w-[100%] lg:w-[49%] p-6 flex flex-col justify-around">
                 <div class="flex justify-between">
                     <div class="flex gap-2">
                         <svg width="22" height="21" viewBox="0 0 22 21" fill="none"
@@ -360,12 +355,12 @@
                     pattern caught my eye, and the fit is perfect. I can see the designer's touch in every aspect of
                     this shirt."
                 </p>
-                <p class="mt-4 pb-8 font-satoshim text-[14px] lg:text-[16px] text-black opacity-60">
+                <p class="mt-4  font-satoshim text-[14px] lg:text-[16px] text-black opacity-60">
                     Posted on August 16, 2023
                 </p>
 
             </div>
-            <div class="border border-black/10 rounded-[20px] w-152 h-60 p-6 flex flex-col justify-around">
+            <div class="border border-black/10 rounded-[20px] w-[100%] lg:w-[49%] p-6 flex flex-col justify-around">
                 <div class="flex justify-between">
                     <div class="flex gap-2">
                         <svg width="22" height="21" viewBox="0 0 22 21" fill="none"
@@ -423,12 +418,12 @@
                     principles but also feels great to wear. It's evident that the designer poured their creativity into
                     making this t-shirt stand out."
                 </p>
-                <p class="mt-4 pb-8 font-satoshim text-[14px] lg:text-[16px] text-black opacity-60">
+                <p class="mt-4  font-satoshim text-[14px] lg:text-[16px] text-black opacity-60">
                     Posted on August 17, 2023
                 </p>
 
             </div>
-            <div class="border border-black/10 rounded-[20px] w-152 h-60 p-6 flex flex-col justify-around">
+            <div class="border border-black/10 rounded-[20px] w-[100%] lg:w-[49%] p-6 flex flex-col justify-around">
                 <div class="flex justify-between">
                     <div class="flex gap-2">
                         <svg width="22" height="21" viewBox="0 0 22 21" fill="none"
@@ -486,12 +481,12 @@
                     volumes about the designer's skill. It's like wearing a piece of art that reflects my passion for
                     both design and fashion."
                 </p>
-                <p class="mt-4 pb-8 font-satoshim text-[14px] lg:text-[16px] text-black opacity-60">
+                <p class="mt-4  font-satoshim text-[14px] lg:text-[16px] text-black opacity-60">
                     Posted on August 18, 2023
                 </p>
 
             </div>
-            <div class="border border-black/10 rounded-[20px] w-152 h-60 p-6 flex flex-col justify-around">
+            <div class="border border-black/10 rounded-[20px] w-[100%] lg:w-[49%] p-6 flex flex-col justify-around">
                 <div class="flex justify-between">
                     <div class="flex gap-2">
                         <svg width="22" height="21" viewBox="0 0 22 21" fill="none"
@@ -548,7 +543,7 @@
                     "I'm not just wearing a t-shirt; I'm wearing a piece of design philosophy. The intricate details and
                     thoughtful layout of the design make this shirt a conversation starter."
                 </p>
-                <p class="mt-4 pb-8 font-satoshim text-[14px] lg:text-[16px] text-black opacity-60">
+                <p class="mt-4  font-satoshim text-[14px] lg:text-[16px] text-black opacity-60">
                     Posted on August 19, 2023
                 </p>
 
@@ -559,14 +554,12 @@
                 Reviews</button>
         </div>
     </div>
-    <h1 class="font-integral font-bold text-[48px] text-center mt-8">You might also like</h1>
-    <!-- <div class="flex justify-between mt-8 w-320">
-        <div class="flex flex-col gap-[8px]">
-            <div class="bg-[#F0EEED] rounded-[20px] w-75 h-75 flex justify-center">
-                <div class="bg-[#F0EEED] rounded-[20px] w-75 h-75 flex justify-center items-center overflow-hidden">
-                    <img class="w-auto h-60" src="{{ asset('images/polo.png') }}" alt="Logo">
+    <h1 class="font-integral font-bold text-[32px] lg:text-[48px] text-center mt-8">You might also like</h1>
+    <div class="flex flex-wrap justify-between gap-4 gap-y-8 lg:gap-8 w-full mt-8">
+        <div class="flex flex-col w-[45%] lg:w-70 gap-[8px]">
+                <div class="bg-[#F0EEED] rounded-[20px] w-full lg:w-70 lg:h-70 p-8 lg:p-10 flex justify-center items-center overflow-hidden">
+                    <img class="w-full object-contain" src="{{ asset('images/polo.png') }}" alt="Logo">
                 </div>
-            </div>
             <label class="font-satoshi font-bold text-[16px] lg:text-[20px]">Polo with Contrast Trims</label>
             <div class="flex gap-[5px]">
                 <svg width="18" height="17" viewBox="0 0 18 17" fill="none"
@@ -596,17 +589,15 @@
                 <label class="font-satoshi text-[14px]">4/<span class="opacity-60">5</span></label>
             </div>
             <div class="flex items-center gap-[8px]">
-                <label class="font-satoshi font-bold text-[24px]">212$</label>
-                <label class="font-satoshi font-bold text-[24px] opacity-40 line-through">254$</label>
+                <label class="font-satoshi font-bold text-[20px] lg:text-[24px]">212$</label>
+                <label class="font-satoshi font-bold text-[20px] lg:text-[24px] opacity-40 line-through">254$</label>
                 <label
                     class="flex items-center justify-center font-satoshi font-medium text-[12px] text-[#FF3333] bg-[#FFEBEB] rounded-[62px] w-[58px] h-[28px]">-20%</label>
             </div>
         </div>
-        <div class="flex flex-col gap-[8px]">
-            <div class="bg-[#F0EEED] rounded-[20px] w-75 h-75 flex justify-center">
-                <div class="bg-[#F0EEED] rounded-[20px] w-75 h-75 flex justify-center items-center overflow-hidden">
-                    <img class="w-auto h-60" src="{{ asset('images/gradient-shirt.png') }}" alt="Logo">
-                </div>
+        <div class="flex flex-col w-[45%] lg:w-70 gap-[8px]">
+            <div class="bg-[#F0EEED] rounded-[20px] w-full lg:w-70 lg:h-70 p-8 lg:p-10 flex justify-center items-center overflow-hidden">
+                    <img class="w-full object-contain" src="{{ asset('images/gradient-shirt.png') }}" alt="Logo">
             </div>
             <label class="font-satoshi font-bold text-[16px] lg:text-[20px]">Gradient Graphic T-shirt</label>
             <div class="flex gap-[5px]">
@@ -636,14 +627,12 @@
                 <label class="font-satoshi text-[14px]">3.5/<span class="opacity-60">5</span></label>
             </div>
             <div class="flex items-center gap-[8px]">
-                <label class="font-satoshi font-bold text-[24px]">145$</label>
+                <label class="font-satoshi font-bold text-[20px] lg:text-[24px]">145$</label>
             </div>
         </div>
-        <div class="flex flex-col gap-[8px]">
-            <div class="bg-[#F0EEED] rounded-[20px] w-75 h-75 flex justify-center">
-                <div class="bg-[#F0EEED] rounded-[20px] w-75 h-75 flex justify-center items-center overflow-hidden">
-                    <img class="w-auto h-60" src="{{ asset('images/polo-t.png') }}" alt="Logo">
-                </div>
+        <div class="flex flex-col w-[45%] lg:w-70 gap-[8px]">
+            <div class="bg-[#F0EEED] rounded-[20px] w-full lg:w-70 lg:h-70 p-8 lg:p-10 flex justify-center items-center overflow-hidden">
+                    <img class="w-full object-contain" src="{{ asset('images/polo-t.png') }}" alt="Logo">
             </div>
             <label class="font-satoshi font-bold text-[16px] lg:text-[20px]">Polo with Tipping Details</label>
             <div class="flex gap-[5px]">
@@ -679,14 +668,12 @@
                 <label class="font-satoshi text-[14px]">4.5/<span class="opacity-60">5</span></label>
             </div>
             <div class="flex items-center gap-[8px]">
-                <label class="font-satoshi font-bold text-[24px]">180$</label>
+                <label class="font-satoshi font-bold text-[20px] lg:text-[24px]">180$</label>
             </div>
         </div>
-        <div class="flex flex-col gap-[8px]">
-            <div class="bg-[#F0EEED] rounded-[20px] w-75 h-75 flex justify-center">
-                <div class="bg-[#F0EEED] rounded-[20px] w-75 h-75 flex justify-center items-center overflow-hidden">
-                    <img class="w-auto h-60" src="{{ asset('images/black-striped-shirt.png') }}" alt="Logo">
-                </div>
+        <div class="flex flex-col w-[45%] lg:w-70 gap-[8px]">
+            <div class="bg-[#F0EEED] rounded-[20px] w-full lg:w-70 lg:h-70 p-8 lg:p-10 flex justify-center items-center overflow-hidden">
+                    <img class="w-full object-contain" src="{{ asset('images/black-striped-shirt.png') }}" alt="Logo">
             </div>
             <label class="font-satoshi font-bold text-[16px] lg:text-[20px]">Black Striped T-shirt</label>
             <div class="flex gap-[5px]">
@@ -723,11 +710,11 @@
                 <label class="font-satoshi text-[14px]">5/<span class="opacity-60">5</span></label>
             </div>
             <div class="flex items-center gap-[8px]">
-                <label class="font-satoshi font-bold text-[24px]">210$</label>
-                <label class="font-satoshi font-bold text-[24px] opacity-40 line-through">252$</label>
+                <label class="font-satoshi font-bold text-[20px] lg:text-[24px]">210$</label>
+                <label class="font-satoshi font-bold text-[20px] lg:text-[24px] opacity-40 line-through">252$</label>
                 <label
                     class="flex items-center justify-center font-satoshi font-medium text-[12px] text-[#FF3333] bg-[#FFEBEB] rounded-[62px] w-[58px] h-[28px]">-20%</label>
             </div>
         </div>
-    </div> -->
+    </div> 
 @endsection
