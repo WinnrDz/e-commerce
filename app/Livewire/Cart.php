49d6c1680eded5cd80005def18fa7fadf10a2fe0
rabah <rabawinnr@gmail.com>
2026-09-17 11:41:42 +0100

    feat: Enhance cart functionality with quantity management

--- app/Livewire/Cart.php
+++ app/Livewire/Cart.php
@@ -4,21 +4,52 @@ namespace App\Livewire;
 
 use Livewire\Component;
 use livewire\Attributes\Layout;
+use App\Models\Variant;
 
 #[Layout('layouts::app')]
 class Cart extends Component
 {
     public $cart;
+    public $variant_id;
+
+
+    public function removeVariant($id) {
+        $this->cart->variants()->detach($id);
+    }
+
+    public function addQuantity($id) {
+        $variant = $this->cart->variants()->where('id',$id)->first();
+
+        $quantity = $variant->pivot->quantity;
+
+        $quantity += 1;
+
+        $this->cart->variants()->updateExistingPivot(
+            $variant->id,
+            ['quantity' => $quantity]
+        );
+    }
+
+    public function subQuantity($id) {
+        $variant = $this->cart->variants()->where('id',$id)->first();
+
+        $quantity = $variant->pivot->quantity;
+
+        $quantity -= 1;
+
+        if ($quantity < 1) $quantity = 1;
+
+        $this->cart->variants()->updateExistingPivot(
+            $variant->id,
+            ['quantity' => $quantity]
+        );
+    }
 
     public function mount()
     {
         $this->cart = \App\Models\Cart::FindOrFail(1);
     }
     
-    public function removeVariant($id) {
-        $this->cart->variants()->detach($id);
-    }
-
     public function render()
     {
         return view('livewire.cart');
